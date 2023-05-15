f0bed6572e3a341c221b02a222d2d6e7c50f89fb
TMMDSS <souzatailam@gmail.com>
2023-05-14 21:50:08 -0300

    update
    adcionei a variavel do header e do main

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const navPul = document.querySelector('#aside')
+const head = document.querySelector('#header')
+const main = document.querySelector('#main')
 
 function enter() {
     navPul.style.left = '0'
