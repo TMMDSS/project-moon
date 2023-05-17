ceb4117a5e2d7a15ae929de4018eb771092709fd
TMMDSS <souzatailam@gmail.com>
2023-05-17 19:42:45 -0300

    update no js

--- index.js
+++ index.js
@@ -9,3 +9,33 @@ function enter() {
 function sair() {
     navPul.style.left = '-25vw'
 }
+
+// AQUI COMEÇA A FUNÇÃO DE CLICK MENU 
+
+let btnKokushibo = document.querySelector('#aKokushibo')
+let btnDoma = document.querySelector('#aDoma')
+let btnAkaza = document.querySelector('#aAkaza')
+let btnHantengu = document.querySelector('#aHantengu')
+let btnGyokko = document.querySelector('#aGyokko')
+let btnGyutaro = document.querySelector('#aGyutaro')
+
+function ação() {
+
+    let showABA = !showABA
+
+    if (showABA) {
+        
+    } else {
+        
+    }
+
+
+
+
+}
+btnKokushibo.addEventListener('click', ação)
+btnDoma.addEventListener('click', ação)
+btnAkaza.addEventListener('click', ação)
+btnHantengu.addEventListener('click', ação)
+btnGyokko.addEventListener('click', ação)
+btnGyutaro.addEventListener('click', ação)
