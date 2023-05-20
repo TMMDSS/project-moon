62c3294ef5ac07655792394dd9c2965b97fea54c
TMMDSS <souzatailam@gmail.com>
2023-05-20 10:24:50 -0300

    update

--- index.js
+++ index.js
@@ -10,50 +10,46 @@ function sair() {
     navPul.style.left = '-25vw'
 }
 
+navPul.addEventListener('mouseenter', enter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // AQUI COMEÇA A FUNÇÃO DE CLICK MENU 
 
-let btnKokushibo = document.querySelector('#aKokushibo')
-let btnDoma = document.querySelector('#aDoma')
-let btnAkaza = document.querySelector('#aAkaza')
-let btnHantengu = document.querySelector('#aHantengu')
-let btnGyokko = document.querySelector('#aGyokko')
-let btnGyutaro = document.querySelector('#aGyutaro')
+const btnKokushibo = document.querySelector('#aKokushibo')
+const btnDoma = document.querySelector('#aDoma')
+const btnAkaza = document.querySelector('#aAkaza')
+const btnHantengu = document.querySelector('#aHantengu')
+const btnGyokko = document.querySelector('#aGyokko')
+const btnGyutaro = document.querySelector('#aGyutaro')
 
-let seckokushibo = document.querySelector('#secKokushibo')
-let secDoma = document.querySelector('#secDoma')
-let secAkaza = document.querySelector('#secAkaza')
-let secHantengu = document.querySelector('#secHantengu')
-let secGyokko = document.querySelector('#secGyokko')
-let secGyutaro = document.querySelector('#secGyutaro')
+const seckokushibo = document.querySelector('#secKokushibo')
+const secDoma = document.querySelector('#secDoma')
+const secAkaza = document.querySelector('#secAkaza')
+const secHantengu = document.querySelector('#secHantengu')
+const secGyokko = document.querySelector('#secGyokko')
+const secGyutaro = document.querySelector('#secGyutaro')
 
 let showABA = false;
 
 
-function ação() {
-    showABA = !showABA
-
-    if (showABA) {
-        seckokushibo.style.display = 'none'
-        secDoma.style.display = 'none'
-        secAkaza.style.display = 'none'
-        secHantengu.style.display = 'none'
-        secGyokko.style.display = 'none'
-        secGyutaro.style.display = 'none'
-        //seckokushibo.classList.add('noneVISE')
-    } else {
-        seckokushibo.style.display = 'flex'
-        secDoma.style.display = 'flex'
-        secAkaza.style.display = 'flex'
-        secHantengu.style.display = 'flex'
-        secGyokko.style.display = 'flex'
-        secGyutaro.style.display = 'flex'
-        //seckokushibo.classList.remove('noneVISE')
-    }
-
-}
-btnKokushibo.addEventListener('click', ação)
-btnDoma.addEventListener('click', ação)
-btnAkaza.addEventListener('click', ação)
-btnHantengu.addEventListener('click', ação)
-btnGyokko.addEventListener('click', ação)
-btnGyutaro.addEventListener('click', ação)
+btnKokushibo.addEventListener('click', transatation)
+btnDoma.addEventListener('click', transatation)
+btnAkaza.addEventListener('click', transatation)
+btnHantengu.addEventListener('click', transatation)
+btnGyokko.addEventListener('click', transatation)
+btnGyutaro.addEventListener('click', transatation)
