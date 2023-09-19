0564ba2cd600e825c128caa34011634fedf92d28
TMMDSS <souzatailam@gmail.com>
2023-09-19 19:11:10 -0300

    js adicionado para barra de pesquisa

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const modal = document.querySelector(".modalContainer")
+const content = document.querySelector("#searchContent");
+const inputSearch = document.querySelector("input[type='search']");
 
-function modall() {
-  modal.classList.add("modalActive")
-  console.log("gagaga")
+let guiasOni = [
+  { file: "cardKokushibo.html", name: "Kokushibo" },
+  { file: "cardDoma.html", name: "Doma" },
+  { file: "cardAkaza.html", name: "Akaza" },
+  { file: "cardHantengu.html", name: "Hantengu" },
+  { file: "cardGyokko.html", name: "Gyokko" },
+  { file: "cardGyutaro.html", name: "Gyutaro" },
+  { file: "cardDaki.html", name: "Daki" },
+  { file: "cardKaigaku.html", name: "Kaigaku" },
+];
+
+let items = guiasOni;
+
+inputSearch.oninput = () => {
+    content.innerHTML = ""
+    
+    items
+    .filter((item) => 
+        item.name.toLowerCase().includes(inputSearch.value.toLowerCase())
+    )
+    .forEach((item) => addHTML(item))
 }
 
-function closeModal() {
-  modal.classList.remove("modalActive")
-
+function addHTML(item) {
+    const div = document.createElement(("div"));
+    div.className = "divOptionSelection";
+    div.innerHTML = `<a href="${item.file}" target="_blank">${item.name}</a>`;
+    content.append(div);
 }
